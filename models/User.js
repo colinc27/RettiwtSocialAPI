99f280f549077fe85e9280fa15dc8b29e0141e8e
colinc27 <colinchristoph27@gmail.com>
2022-06-08 18:03:50 -0700

    Working User Model

--- models/User.js
+++ models/User.js
@@ -1,8 +1,13 @@
 const { Schema, model } = require('mongoose');
 
+var validateEmail = function(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
+
 const UserSchema = new Schema(
   {
-    userName: {
+    username: {
       type: String,
       required: true,
       trim: true,
@@ -15,7 +20,9 @@ const UserSchema = new Schema(
         validate: {
             validator: () => Promise.resolve(false),
             message: 'Email validation failed'
-          }
+          },
+          validate: [validateEmail, 'Please fill a valid email address'],
+          match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
       },
     thoughts: [
       {
